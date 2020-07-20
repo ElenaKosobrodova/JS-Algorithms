ab9c477ec9ba31545961e5d91439c7398fbd50a1
Elena <kosobrodova@fastmail.com.au>
2020-07-20 15:25:48 +1000

    roman to integer

---
+++ roman-to-integer.js
@@ -0,0 +1,29 @@
+var romanToInt = function (s) {
+  var base = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+  var exc = {
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900
+  };
+  var res = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] + s[i + 1] in exc) {
+      res += base[s[i + 1]] - base[s[i]];
+      i++;
+    } else {
+      res += base[s[i]];
+    }
+  }
+  return res;
+};
